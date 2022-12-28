50a0d731c269f9ad927db561f16df46579a0a16f
rajeshceg3 <rajeshceg3@gmail.com>
2022-12-28 06:24:35 +0530

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { SquareComponent } from './square/square.component';
+import { BoardComponent } from './board/board.component';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, SquareComponent ],
+  declarations: [ AppComponent, HelloComponent, SquareComponent, BoardComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
 })
 export class SquareComponent  {
 
-  constructor() { 
-    setInterval(()=> this.rando = Math.random(),500)
+@Input() value: 'X' | 'O'; 
+
 }
